f66fd701122f9d5586f67e7989e6654e2364eb8b
Kyle Wilson <kwilson@ooyala.com>
2015-08-18 18:21:24 -0700

    fixed bug in path

--- clear-cache.sh
+++ clear-cache.sh
@@ -4,4 +4,4 @@ REPOTYPE="${myname%%-*}"
 echo "${myname} Status:"
 
 echo "Deleting .sincedb file..."
-rm /root/atlantis-analytics/logstash-atlantis-$REPOTYPE/.sincedb*
+rm /root/atlantis-analytics/.sincedb*

de0cd133a239787db9b947babe27acd84f69c5ae
Kyle Wilson <kwilson@ooyala.com>
2015-08-31 15:35:50 -0700

    changed to use bash not sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ REPOTYPE="${myname%%-*}"
 echo "${myname} Run:"
 
 echo "Starting logstash run script..."
-sh /root/atlantis-analytics/logstash-atlantis-$REPOTYPE/run.sh
+bash /root/atlantis-analytics/logstash-atlantis-$REPOTYPE/run.sh
